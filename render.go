50e0b5576f0b2b297ea9bea31d9ab510a0dda5b7
Derrick J. Wippler <thrawn01@gmail.com>
2018-12-20 17:37:56 -0600

    Added some color to the charts

--- render.go
+++ render.go
@@ -7,6 +7,21 @@ import (
 	"sort"
 )
 
+func counterToColor(counter string) string {
+	switch counter {
+	case "messages":
+		return "blue"
+	case "positive":
+		return "green"
+	case "negative":
+		return "red"
+	case "emoji":
+		return "yellow"
+	default:
+		return "blue"
+	}
+}
+
 func RenderPercentage(store Storer, w io.Writer, timeRange *TimeRange, channelID, counter string) error {
 	totals, err := store.PercentageByUser(timeRange, channelID, counter)
 	if err != nil {
@@ -29,7 +44,7 @@ func RenderPercentage(store Storer, w io.Writer, timeRange *TimeRange, channelID
 		return dps[i].Value < dps[j].Value
 	})
 
-	return renderBarChart(w, dps)
+	return renderBarChart(w, dps, counterToColor(counter))
 }
 
 func RenderSum(store Storer, w io.Writer, timeRange *TimeRange, channelID, counter string) error {
@@ -50,12 +65,13 @@ func RenderSum(store Storer, w io.Writer, timeRange *TimeRange, channelID, count
 		dps = append(dps, chart.Value{Label: item.User, Value: float64(item.Sum)})
 	}
 
-	return renderBarChart(w, dps)
+	return renderBarChart(w, dps, counterToColor(counter))
 }
 
-func renderBarChart(w io.Writer, bars []chart.Value) error {
+func renderBarChart(w io.Writer, bars []chart.Value, color string) error {
+
 	sbc := chart.BarChart{
-		ColorPalette: RedColorPalette{},
+		ColorPalette: CustomColors{Colors: barColors[color]},
 		Background: chart.Style{
 			Show: true,
 			Padding: chart.Box{
@@ -91,42 +107,64 @@ func FloatFormatter(v interface{}) string {
 	return chart.FloatValueFormatterWithFormat(v, "%.f")
 }
 
-type RedColorPalette struct{}
+type CustomColors struct {
+	Colors []drawing.Color
+}
 
-func (dp RedColorPalette) BackgroundColor() drawing.Color {
+func (c CustomColors) BackgroundColor() drawing.Color {
 	return chart.DefaultBackgroundColor
 }
 
-func (dp RedColorPalette) BackgroundStrokeColor() drawing.Color {
+func (c CustomColors) BackgroundStrokeColor() drawing.Color {
 	return chart.DefaultBackgroundStrokeColor
 }
 
-func (dp RedColorPalette) CanvasColor() drawing.Color {
+func (c CustomColors) CanvasColor() drawing.Color {
 	return chart.DefaultCanvasColor
 }
 
-func (dp RedColorPalette) CanvasStrokeColor() drawing.Color {
+func (c CustomColors) CanvasStrokeColor() drawing.Color {
 	return chart.DefaultCanvasStrokeColor
 }
 
-func (dp RedColorPalette) AxisStrokeColor() drawing.Color {
+func (c CustomColors) AxisStrokeColor() drawing.Color {
 	return chart.DefaultAxisColor
 }
 
-func (dp RedColorPalette) TextColor() drawing.Color {
+func (c CustomColors) TextColor() drawing.Color {
 	return chart.DefaultTextColor
 }
 
+func (c CustomColors) GetSeriesColor(index int) drawing.Color {
+	finalIndex := index % len(c.Colors)
+	return c.Colors[finalIndex]
+}
+
 var (
-	barColors = []drawing.Color{
-		{R: 255, G: 179, B: 179, A: 255}, // Light red
-		{R: 255, G: 128, B: 128, A: 255},
-		{R: 255, G: 77, B: 77, A: 255},
-		{R: 255, G: 0, B: 0, A: 255}, // Dark red
+	barColors = map[string][]drawing.Color{
+		"red": {
+			{R: 237, G: 192, B: 198, A: 255}, // Light
+			{R: 219, G: 136, B: 141, A: 255},
+			{R: 203, G: 86, B: 100, A: 255},
+			{R: 208, G: 2, B: 27, A: 255}, // Dark
+		},
+		"green": {
+			{R: 209, G: 239, B: 255, A: 255}, // Light
+			{R: 167, G: 223, B: 199, A: 255},
+			{R: 129, G: 207, B: 170, A: 255},
+			{R: 73, G: 178, B: 121, A: 255}, // Dark
+		},
+		"blue": {
+			{R: 212, G: 217, B: 236, A: 255}, // Light
+			{R: 168, G: 180, B: 217, A: 255},
+			{R: 130, G: 146, B: 201, A: 255},
+			{R: 60, G: 83, B: 167, A: 255}, // Dark
+		},
+		"yellow": {
+			{R: 252, G: 242, B: 212, A: 255}, // Light
+			{R: 250, G: 230, B: 170, A: 255},
+			{R: 246, G: 217, B: 134, A: 255},
+			{R: 241, G: 192, B: 69, A: 255}, // Dark
+		},
 	}
 )
-
-func (dp RedColorPalette) GetSeriesColor(index int) drawing.Color {
-	finalIndex := index % len(barColors)
-	return barColors[finalIndex]
-}
